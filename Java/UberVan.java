0e0eae2b5c8c71f9f3e2d2ca92a7abe120f6c2a4
albertusortiz <alberto.ortiz.vargas@gmail.com>
2020-12-04 13:58:51 -0600

    Creando los ultimos cambios en Java.

--- Java/UberVan.java
+++ Java/UberVan.java
@@ -2,15 +2,27 @@ import java.util.ArrayList;
 import java.util.Map;
 
 class UberVan extends Car {
-    //Map<String, ArrayList<String,Integer>> typeCarAccepted;
     Map<String, Map<String,Integer>> typeCarAccepted;
     ArrayList<String> seatsMaterial;
 
-    public UberVan(String license, Account driver,
-    Map<String, Map<String,Integer>> typeCarAccepted,
-    ArrayList<String> seatsMaterial) {
+    // public UberVan(String license, Account driver,
+    // Map<String, Map<String,Integer>> typeCarAccepted,
+    // ArrayList<String> seatsMaterial) {
+    //     super(license, driver);
+    //     this.typeCarAccepted = typeCarAccepted;
+    //     this.seatsMaterial = seatsMaterial;
+    // }
+
+    public UberVan(String license, Account driver) {
         super(license, driver);
-        this.typeCarAccepted = typeCarAccepted;
-        this.seatsMaterial = seatsMaterial;
+    }
+
+    @Override
+    public void setPassenger(Integer passenger) {
+        if(passenger == 6) {
+            this.passenger = passenger;
+        } else {
+            System.out.println("Necesitas asignar 6 pasajeros.");
+        }
     }
 }
